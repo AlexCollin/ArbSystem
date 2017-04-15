df6c9c1e74441f06ebcf33e2db34e55c8bf88bb9
Alex Collin <alex@collin.su>
2017-04-15 20:44:59 +0300

    EPC CR

--- app/admin/statistics/subaccounts.rb
+++ app/admin/statistics/subaccounts.rb
@@ -46,26 +46,47 @@ ActiveAdmin.register Click, as: 'SubAccount' do
     column scope
     column :clicks
     column :hits
+    column 'CR' do |row|
+      all = row.wait + row.approve + row.decline
+      if all > 0
+        span (row.approve.to_f / all.to_f * 100).round(2).to_s + '%'
+      else
+        span 0.to_s + '%'
+      end
+    end
+    column 'EPC' do |row|
+      span (row.money_approve.to_f / row.clicks.to_f).round(2).to_s + '₽'
+    end
+    column 'EPC2' do |row|
+      all = row.money_approve.to_f + row.money_wait.to_f + row.money_decline.to_f
+      span (all.to_f / row.clicks.to_f).round(2).to_s + '₽'
+    end
     column :wait
     column :approve
     column :decline
-    column :money_wait
-    column :money_approve
-    column :money_decline
+    column :money_wait do |row|
+      span row.money_wait.to_s + '₽'
+    end
+    column :money_approve do |row|
+      span row.money_approve.to_s + '₽'
+    end
+    column :money_decline do |row|
+      span row.money_decline.to_s + '₽'
+    end
   end
 
   controller do
     def scoped_collection
       Click.left_joins(:conversions)
           .select(
-                  'sum(case when clicks.amount > 0 then 1 else 0 end) clicks',
-                  'sum(case when clicks.amount > 0 then clicks.amount else 0 end) hits',
-                  'sum(case when conversions.status = 0 then 1 else 0 end) wait',
-                  'sum(case when conversions.status = 1 then 1 else 0 end) approve',
-                  'sum(case when conversions.status = 2 then 1 else 0 end) decline',
-                  'sum(case when conversions.status = 1 then conversions.ext_payout::int else 0 end) money_wait',
-                  'sum(case when conversions.status = 2 then conversions.ext_payout::int else 0 end) money_approve',
-                  'sum(case when conversions.status = 3 then conversions.ext_payout::int else 0 end) money_decline'
+              'sum(case when clicks.amount > 0 then 1 else 0 end) clicks',
+              'sum(case when clicks.amount > 0 then clicks.amount else 0 end) hits',
+              'sum(case when conversions.status = 0 then 1 else 0 end) wait',
+              'sum(case when conversions.status = 1 then 1 else 0 end) approve',
+              'sum(case when conversions.status = 2 then 1 else 0 end) decline',
+              'sum(case when conversions.status = 0 then conversions.ext_payout::int else 0 end) money_wait',
+              'sum(case when conversions.status = 1 then conversions.ext_payout::int else 0 end) money_approve',
+              'sum(case when conversions.status = 2 then conversions.ext_payout::int else 0 end) money_decline'
           )
     end
   end
